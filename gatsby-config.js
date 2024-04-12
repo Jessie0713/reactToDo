e4b27d0963f767676478c48e4434c449b5012162
System Administrator <root@xuxiangjiedeMacBook-Pro.local>
2024-04-12 13:41:17 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `todo list`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
